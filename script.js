136c9bdbdb0f2ade136473700ff6029a6cbb446a
Luka <luka.luka.luka93@gmail.com>
2024-12-20 12:25:04 -0800

    made changes in the html and css with the unordered list of the languages offered, put them all in one ul and divided into  3 columns

---
+++ script.js
@@ -0,0 +1,6 @@
+const arrowDownButtonTranslate = document.querySelector('#arrow-down');
+const chooseLanguageWindow = document.querySelector('.choose-language');
+const searchLanguage = document.querySelector('#search');
+const selectLanguageMenu = document.querySelector('.select-language select');
+const languagesOffered = document.querySelector('.languages-to-choose');
+console.log(languagesOffered)
