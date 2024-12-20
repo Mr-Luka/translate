6458941753929146c750b6c3dd765293de670042
Luka <luka.luka.luka93@gmail.com>
2024-12-20 12:51:23 -0800

    selected all the elements from the upper and lower window in js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const arrowDownButtonTranslate = document.querySelector('#arrow-down');
+// Upper window where user will type the words or click buttons
+const arrowDownButtonTranslateOptions = document.querySelector('#arrow-down');
 const chooseLanguageWindow = document.querySelector('.choose-language');
 const searchLanguage = document.querySelector('#search');
 const selectLanguageMenu = document.querySelector('.select-language select');
-const languagesOffered = document.querySelector('.languages-to-choose');
-console.log(languagesOffered)
+const languagesOffered = document.querySelector('.offered-langauges-while-typing');
+const inputBoxTextArea = document.querySelector('.input-box');
+const typeWords = document.querySelector('#input-text');
+const xDeleteBtn = document.querySelector('.x-delete');
+const microphone = document.querySelector('#microphone');
+const speakerType = document.querySelector('#speaker-type');
+const titleOptions = document.querySelectorAll('.titles ul li');
+
+// lower window where user will get the results of the translate and also click buttons for sound or copy etc
+const titleOptions2= document.querySelectorAll('.titles-translate ul li');
+const arrowDownButton2 = document.querySelector('#arrow-down-translate');
+const chooseLanguageWindow2 = document.querySelector('.choose-language-translate');
+const searchLanguage2 = document.querySelector('#search-language');
+const selectLanguageMenu2 = document.querySelector('.select-language-translate select');
+const languagesOffered2 = document.querySelector('.offered-langauges-while-typing-translate');
+const translateBox = document.querySelector('.translate-box');
+const translateLive = document.querySelector('.translate p');
+const speaker = document.querySelector('#speaker');
+const copy = document.querySelector('#copy');
+const swapLanguages = document.querySelector('#swap-logo');
